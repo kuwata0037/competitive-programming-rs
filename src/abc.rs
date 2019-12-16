3a019d7c9dcbbfed1e9471b17fc066387d11f8bc
kuwata0037 <kuwata0037@gmail.com>
2019-12-16 21:49:31 +0900

    Solve abc085-b

--- src/abc.rs
+++ src/abc.rs
@@ -1,5 +1,6 @@
 mod abc081;
 mod abc083;
+mod abc085;
 mod abc086;
 mod abc087;
 mod abc088;
---
+++ src/abc/abc085/b.rs
@@ -0,0 +1,81 @@
+use std::collections::HashSet;
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let mut s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+macro_rules! stdin {
+    () => {{
+        use std::io::Read;
+        let mut s = String::new();
+        std::io::stdin().read_to_string(&mut s).unwrap();
+        s
+    }};
+}
+
+#[allow(dead_code)]
+fn main() {
+    println!("{}", solve(&stdin!()));
+}
+
+fn solve(src: &str) -> String {
+    input!(source = src, n: usize, d: [u32; n]);
+    let d: Vec<u32> = d;
+    d.into_iter().collect::<HashSet<u32>>().len().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(solve("4\n10\n8\n8\n6"), "3");
+        assert_eq!(solve("3\n15\n15\n15"), "1");
+        assert_eq!(solve("7\n50\n30\n50\n100\n50\n80\n30"), "4");
+    }
+}
