a825fbab44b96fe58170b977a1a0e632e3a86e67
kuwata0037 <kuwata0037@gmail.com>
2020-06-17 23:31:21 +0900

    Resolve abc087-b

--- src/abc/abc087/b.rs
+++ src/abc/abc087/b.rs
@@ -1,80 +1,28 @@
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let mut s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-}
+use itertools::iproduct;
+use proconio::{input, source::auto};
+use std::io::BufRead;
 
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
-}
-
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
-}
-
-macro_rules! stdin {
-    () => {{
-        use std::io::Read;
-        let mut s = String::new();
-        std::io::stdin().read_to_string(&mut s).unwrap();
-        s
-    }};
-}
-
-#[allow(dead_code)]
 fn main() {
-    println!("{}", solve(&stdin!()));
+    let stdin = std::io::stdin();
+    println!("{}", solve(auto::AutoSource::new(stdin.lock())));
 }
 
-fn solve(src: &str) -> String {
-    input!(source = src, a: u32, b: u32, c: u32, x: u32);
+fn solve<T, R>(source: T) -> usize
+where
+    T: Into<auto::AutoSource<R>>,
+    R: BufRead,
+{
+    input! {
+        from source.into(),
+        a: u32,
+        b: u32,
+        c: u32,
+        x: u32,
+    };
 
-    let mut cnt = 0;
-    for i in 0..a + 1 {
-        for j in 0..b + 1 {
-            for k in 0..c + 1 {
-                if i * 500 + j * 100 + k * 50 == x {
-                    cnt += 1;
-                }
-            }
-        }
-    }
-    cnt.to_string()
+    iproduct!(0..=a, 0..=b, 0..=c)
+        .filter(|(a, b, c)| a * 500 + b * 100 + c * 50 == x)
+        .count()
 }
 
 #[cfg(test)]
@@ -83,8 +31,16 @@ mod tests {
 
     #[test]
     fn case1() {
-        assert_eq!(solve("2\n2\n2\n100"), "2");
-        assert_eq!(solve("5\n1\n0\n150"), "0");
-        assert_eq!(solve("30\n40\n50\n6000"), "213");
+        assert_eq!(solve("2 2 2 100"), 2);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(solve("5 1 0 150"), 0);
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(solve("30 40 50 6000"), 213);
     }
 }
