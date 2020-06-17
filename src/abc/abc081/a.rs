350927bf219f8d9609c04554f21ceadddb23d78c
kuwata0037 <kuwata0037@gmail.com>
2020-06-17 22:40:23 +0900

    Resolve abc081-a

--- src/abc/abc081/a.rs
+++ src/abc/abc081/a.rs
@@ -1,70 +1,22 @@
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let mut s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-}
+use proconio::{input, marker::Chars, source::auto};
+use std::io::BufRead;
 
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
-}
-
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
-}
-
-macro_rules! stdin {
-    () => {{
-        use std::io::Read;
-        let mut s = String::new();
-        std::io::stdin().read_to_string(&mut s).unwrap();
-        s
-    }};
-}
-
-#[allow(dead_code)]
 fn main() {
-    println!("{}", solve(&stdin!()));
+    let stdin = std::io::stdin();
+    println!("{}", solve(auto::AutoSource::new(stdin.lock())));
 }
 
-fn solve(src: &str) -> String {
-    input!(source = src, c: chars);
-    let c: Vec<char> = c;
-    c.into_iter().filter(|s| *s == '1').count().to_string()
+fn solve<T, R>(source: T) -> usize
+where
+    T: Into<auto::AutoSource<R>>,
+    R: BufRead,
+{
+    input! {
+        from source.into(),
+        chars: Chars
+    };
+    let chars: Vec<char> = chars;
+    chars.iter().filter(|char| **char == '1').count()
 }
 
 #[cfg(test)]
@@ -73,7 +25,11 @@ mod tests {
 
     #[test]
     fn case1() {
-        assert_eq!(solve("101"), "2");
-        assert_eq!(solve("000"), "0");
+        assert_eq!(solve("101"), 2);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(solve("000"), 0);
     }
 }
