4f74387b91d9ad4f20b9e304a34e6c6e549a157f
kuwata0037 <kuwata0037@gmail.com>
2020-06-18 21:31:42 +0900

    Resolve abc085-c

--- src/abc/abc085/c.rs
+++ src/abc/abc085/c.rs
@@ -1,74 +1,26 @@
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let mut s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-}
+use proconio::{input, source::auto};
+use std::io::BufRead;
 
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
-}
-
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
-}
-
-macro_rules! stdin {
-    () => {{
-        use std::io::Read;
-        let mut s = String::new();
-        std::io::stdin().read_to_string(&mut s).unwrap();
-        s
-    }};
-}
-
-#[allow(dead_code)]
 fn main() {
-    println!("{}", solve(&stdin!()));
+    let stdin = std::io::stdin();
+    println!("{}", solve(auto::AutoSource::new(stdin.lock())));
 }
 
-fn solve(src: &str) -> String {
-    input!(source = src, n: u32, y: u32);
+fn solve<T, R>(source: T) -> String
+where
+    T: Into<auto::AutoSource<R>>,
+    R: BufRead,
+{
+    input! {
+        from source.into(),
+        n: u32,
+        y: u32,
+    }
 
-    for i in 0..n + 1 {
-        for j in 0..n + 1 - i {
-            let k = n - i - j;
-            if 10000 * i + 5000 * j + 1000 * k == y {
-                return format!("{} {} {}", i, j, k);
+    for i in 0..=n {
+        for j in 0..=n - i {
+            if 10000 * i + 5000 * j + 1000 * (n - i - j) == y {
+                return format!("{} {} {}", i, j, n - i - j);
             }
         }
     }
@@ -83,8 +35,20 @@ mod tests {
     #[test]
     fn case1() {
         assert_eq!(solve("9 45000"), "0 9 0");
+    }
+
+    #[test]
+    fn case2() {
         assert_eq!(solve("20 196000"), "-1 -1 -1");
+    }
+
+    #[test]
+    fn case3() {
         assert_eq!(solve("1000 1234000"), "2 54 944");
+    }
+
+    #[test]
+    fn case4() {
         assert_eq!(solve("2000 20000000"), "2000 0 0");
     }
 }
