98957608dc8bac5594a0536dcf548432e331e209
kuwata0037 <kuwata0037@gmail.com>
2020-06-18 20:20:18 +0900

    Resolve abc083-b

--- src/abc/abc083/b.rs
+++ src/abc/abc083/b.rs
@@ -1,86 +1,32 @@
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let mut s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-}
+use proconio::{input, source::auto};
+use std::io::BufRead;
 
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
-}
-
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
-}
-
-macro_rules! stdin {
-    () => {{
-        use std::io::Read;
-        let mut s = String::new();
-        std::io::stdin().read_to_string(&mut s).unwrap();
-        s
-    }};
-}
-
-#[allow(dead_code)]
 fn main() {
-    println!("{}", solve(&stdin!()));
+    let stdin = std::io::stdin();
+    println!("{}", solve(auto::AutoSource::new(stdin.lock())));
 }
 
-fn solve(src: &str) -> String {
-    input!(source = src, n: u32, a: u32, b: u32);
-
-    let mut ans = 0;
-    for i in 0..n + 1 {
-        let sum = sum_digits(i);
-        if a <= sum && sum <= b {
-            ans += i;
-        }
+fn solve<T, R>(source: T) -> u32
+where
+    T: Into<auto::AutoSource<R>>,
+    R: BufRead,
+{
+    input! {
+        from source.into(),
+        n: u32,
+        a: u32,
+        b: u32,
     }
-    ans.to_string()
-}
-
-fn sum_digits(mut value: u32) -> u32 {
-    let mut sum = 0;
-    while value > 0 {
-        sum += value % 10;
-        value /= 10;
-    }
-    sum
+    (0..=n)
+        .filter(|num| {
+            let sum = num
+                .to_string()
+                .chars()
+                .map(|char| char.to_digit(10).unwrap())
+                .sum();
+            a <= sum && sum <= b
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -89,8 +35,16 @@ mod tests {
 
     #[test]
     fn case1() {
-        assert_eq!(solve("20 2 5"), "84");
-        assert_eq!(solve("10 1 2"), "13");
-        assert_eq!(solve("100 4 16"), "4554");
+        assert_eq!(solve("20 2 5"), 84);
+    }
+
+    #[test]
+    fn case2() {
+        assert_eq!(solve("10 1 2"), 13);
+    }
+
+    #[test]
+    fn case3() {
+        assert_eq!(solve("100 4 16"), 4554);
     }
 }
